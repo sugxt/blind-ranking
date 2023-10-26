0cc64d504ab121eb6a5bc4c1e1ca6b51d1208bda
Sugat Poudyal <official.sugat@gmail.com>
2023-10-26 19:44:37 +0545

    Unique Generations and Fixes
    - the generations are always unique
    - can't generate if pending assessments

--- app.js
+++ app.js
@@ -4,18 +4,27 @@ const genBtn = document.getElementById('genElem');
 const blackBox = document.querySelector('.blackBox');
 
 let prevRand = []
+let isBlackBoxEmpty = true;
 
 function randGen(){
+    if(prevRand.length > 5){
+        return prevRand[prevRand.length-1]
+    }
     let rand;
-    do {
-        rand = Math.floor(Math.random()*4)+1;
-    }while(prevRand.includes(rand))
-    prevRand.push(rand)
-    return rand;
+    rand = Math.floor(Math.random() * 5) + 1
+    if(!prevRand.includes(rand)){
+        prevRand.push(rand)
+        return rand
+    } else{
+        return randGen()
+    }
 }
 
 function divGenerator() {
     genBtn.addEventListener('click', () => {
+        if (!isBlackBoxEmpty) {
+            return;
+        }
         const rand = randGen()
         console.log(rand);
         const newBox = document.createElement('div');
@@ -24,7 +33,7 @@ function divGenerator() {
         newBox.style.backgroundImage = `url("/images/albums/${rand}.jpg")`; // Add the 'imgBox' class and 'dragging' class to make it draggable
         newBox.setAttribute('draggable', true);
         blackBox.appendChild(newBox);
-
+        isBlackBoxEmpty = false;
         // Add dragstart event listener for the newly generated div
         newBox.addEventListener('dragstart', (e) => {
             if (newBox.getAttribute('draggable') === "false") {
@@ -62,6 +71,7 @@ whiteBoxes.forEach(container => {
             container.appendChild(droppedBox);
             droppedBox.setAttribute('draggable', false); // Make the dropped box non-draggable
             droppedBox.classList.remove("dragging"); // Remove the "dragging" class
+            isBlackBoxEmpty = true;
         }
     });
 });
